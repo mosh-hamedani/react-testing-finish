877b682434c64f14c92151215ae10e9ccba373b6
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-01 09:51:36 -0800

    Simplifying tests

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -1,31 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import TermsAndConditions from '../../src/components/TermsAndConditions';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
 
-describe('TermsAndConditions', () => {
-  it('should render with correct text and initial state', () => {
+describe("TermsAndConditions", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />);
 
-    const heading = screen.getByRole('heading');
-    expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent('Terms & Conditions');
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button"),
+    };
+  };
 
-    const checkbox = screen.getByRole('checkbox');
-    expect(checkbox).toBeInTheDocument();
+  it("should render with correct text and initial state", () => {
+    const { heading, checkbox, button } = renderComponent();
+
+    expect(heading).toHaveTextContent("Terms & Conditions");
     expect(checkbox).not.toBeChecked();
-
-    const button = screen.getByRole('button');
-    expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
   });
 
-  it('should enable the button when the checkbox is checked', async () => {
-    render(<TermsAndConditions />);
+  it("should enable the button when the checkbox is checked", async () => {
+    const { checkbox, button } = renderComponent();
 
-    const checkbox = screen.getByRole('checkbox');
     const user = userEvent.setup();
     await user.click(checkbox);
 
-    expect(screen.getByRole('button')).toBeEnabled();
-  })
-})
+    expect(button).toBeEnabled();
+  });
+});
