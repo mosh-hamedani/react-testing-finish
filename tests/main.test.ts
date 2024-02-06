7c66a06a5ebcc4fa33c9924c717fb846840dbfa7
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-06 09:41:35 -0800

    Generating fake data

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,11 @@
 import { it, expect, describe } from 'vitest';
+import { faker } from '@faker-js/faker';
 
 describe('group', () => {
-  it('should', async () => {
-    const response = await fetch('/categories');
-    const data = await response.json();
-    console.log(data)
-    expect(data).toHaveLength(3);
+  it('should', () => {
+    console.log({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price({ min: 1, max: 100 })
+    })
   })
 })
