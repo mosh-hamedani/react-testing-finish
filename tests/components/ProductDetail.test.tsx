cfe471cf2dd457f5c13f8d66ea7517f4a0be3fa0
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-06 10:58:10 -0800

    Exercise: Mocking data

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,15 +1,28 @@
 import { render, screen } from '@testing-library/react'
 import ProductDetail from '../../src/components/ProductDetail'
-import { products } from '../mocks/data';
 import { server } from '../mocks/server';
 import { http, HttpResponse } from 'msw';
+import { db } from '../mocks/db';
 
 describe('ProductDetail', () => {
-  it('should render the list of products', async () => {
-    render(<ProductDetail productId={1} />);
+  let productId: number; 
 
-    expect(await screen.findByText(new RegExp(products[0].name))).toBeInTheDocument();
-    expect(await screen.findByText(new RegExp(products[0].price.toString()))).toBeInTheDocument();
+  beforeAll(() => {
+    const product = db.product.create();
+    productId = product.id;
+  })
+
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId }}})
+  })
+
+  it('should render product details', async () => {
+    const product = db.product.findFirst({ where: { id: { equals: productId }}})
+
+    render(<ProductDetail productId={productId} />);
+
+    expect(await screen.findByText(new RegExp(product!.name))).toBeInTheDocument();
+    expect(await screen.findByText(new RegExp(product!.price.toString()))).toBeInTheDocument();
   });
 
   it('should render message if product not found', async () => {
