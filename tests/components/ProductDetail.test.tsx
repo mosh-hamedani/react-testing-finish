0627925b9acb03a22dd3a856cec8ca7a3214e502
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-06 11:11:22 -0800

    Exercise: Testing errors

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -40,4 +40,12 @@ describe('ProductDetail', () => {
     const message = await screen.findByText(/invalid/i);
     expect(message).toBeInTheDocument();
   })
+
+  it('should render an error if data fetching fails', async () => {
+    server.use(http.get('/products/1', () => HttpResponse.error()));
+    
+    render(<ProductDetail productId={1} />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  })
 })
