86ba9db87e62d351eaf2832a8d15e9c818fcf5dc
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-09 16:06:59 -0800

    Exercise: Testing validation rules

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -111,4 +111,50 @@ describe("ProductForm", () => {
       expect(error).toHaveTextContent(errorMessage);
     }
   );
+
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "negative",
+      price: -1,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/,
+    },
+    {
+      scenario: "not a number",
+      price: "a",
+      errorMessage: /required/,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad } = renderComponent();
+
+      const form = await waitForFormToLoad();
+      const user = userEvent.setup();
+      await user.type(form.nameInput, "a");
+      if (price !== undefined)
+        await user.type(form.priceInput, price.toString());
+      await user.click(form.categoryInput);
+      const options = screen.getAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.submitButton);
+
+      const error = screen.getByRole("alert");
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    }
+  );
 });
