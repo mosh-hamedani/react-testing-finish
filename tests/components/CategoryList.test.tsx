3b0a956f83052d7c983cdf5a373e6683f009c69e
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-13 16:11:50 -0800

    Exercise: Testing components that use Redux

---
+++ tests/components/CategoryList.test.tsx
@@ -0,0 +1,68 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import CategoryList from "../../src/components/CategoryList";
+import { Category } from "../../src/entities";
+import ReduxProvider from "../../src/providers/ReduxProvider";
+import { db } from "../mocks/db";
+import { simulateDelay, simulateError } from "../utils";
+
+describe("CategoryList", () => {
+  const categories: Category[] = [];
+
+  beforeAll(() => {
+    [1, 2].forEach(() => {
+      const category = db.category.create();
+      categories.push(category);
+    });
+  });
+
+  afterAll(() => {
+    const categoryIds = categories.map((c) => c.id);
+    db.category.deleteMany({
+      where: { id: { in: categoryIds } },
+    });
+  });
+
+  const renderComponent = () => {
+    render(
+      <ReduxProvider>
+        <CategoryList />
+      </ReduxProvider>
+    );
+  };
+
+  it("should render a list of categories", async () => {
+    renderComponent();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/loading/i)
+    );
+
+    categories.forEach((category) => {
+      expect(
+        screen.getByText(category.name)
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("should render a loading message when fetching categories", () => {
+    simulateDelay("/categories");
+
+    renderComponent();
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+
+  it("should render an error message if fetching categories fails", async () => {
+    simulateError("/categories");
+
+    renderComponent();
+
+    expect(
+      await screen.findByText(/error/i)
+    ).toBeInTheDocument();
+  });
+});
