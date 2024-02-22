ffc2093c9950f80d5f00e44f4628e6f5395ae177
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-02-22 08:45:28 -0800

    Exercise: Testing routes with parameters

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { navigateTo } from './utils'
+import { db } from './mocks/db';
 
 describe('Router', () => {
   it('should render the home page for /', () => {
@@ -12,5 +13,15 @@ describe('Router', () => {
     navigateTo('/products');
 
     expect(screen.getByRole('heading', { name: /products/i })).toBeInTheDocument();
+  })
+
+  it('should render the product details page for /products/:id', async () => {
+    const product = db.product.create();
+
+    navigateTo('/products/' + product.id);
+
+    expect(await screen.findByRole('heading', { name: product.name })).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id }}});
   })
 })
